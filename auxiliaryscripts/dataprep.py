e87ab37ff360dd388550a35f22522fbe084a353e
Álvaro Saldanha <alvaroqsaldanha@gmail.com>
2022-05-11 10:26:04 +0900

    Updating data

--- auxiliaryscripts/dataprep.py
+++ auxiliaryscripts/dataprep.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import numpy as np
 
 pg = pd.read_csv('penguins.csv')
 tosave = pg.drop(['rowid'], axis=1)
 tosave.dropna(subset=['bill_depth_mm'], inplace=True)
+tosave.replace(np.nan, 'other', regex=True, inplace = True)
 tosave.to_csv('penguins_prep.csv',index=False)
